23011bf3813ba2de953cffb62f85ccb6b6a0313b
pomponiosimonegit config --global usern.email pomponio671@gmail.com <Pomponio671@gmail.com>
2024-06-26 17:21:37 +0200

    aggiunta file json

--- src/components/SingleBook.jsx
+++ src/components/SingleBook.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import Card from 'react-bootstrap/Card';
-
+import { Container, Row, Col } from 'react-bootstrap';
 class SingleBook extends Component {
   constructor(props) {
     super(props);
@@ -21,22 +21,28 @@ class SingleBook extends Component {
     const { selected } = this.state;
 
    
-    const cardStyle = {
+    const cardStyle = { width:"18rem",
       border: selected ? '4px solid red' : '3px solid black',
       marginBottom: '10px'
     };
 
     return (
+      <Container>
+  <Row className='justify-content-center'>
+    <Col md="auto">
       <Card style={cardStyle} onClick={this.handleClick}>
         <Card.Img variant="top" src={process.env.PUBLIC_URL + book.cover} />
         <Card.Body>
           <Card.Title>{book.title}</Card.Title>
           <Card.Text>
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla dolorum laborum, molestias temporibus pariatur libero atque fuga quod nihil impedit aliquid nisi est possimus magni omnis cumque, aut velit voluptas?
-            Dolor, dolorem! Quos qui mollitia, quibusdam sequi aliquam facere eum, voluptatem expedita cupiditate fugit odio tempore atque distinctio. Rem, fugit exercitationem quos maxime veniam modi eaque nihil! Illo, culpa quasi?
           </Card.Text>
         </Card.Body>
       </Card>
+    </Col>
+  </Row>
+</Container>
+      
     );
   }
 }
